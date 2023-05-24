64c21b93c3906fe6058c26e89a6f8b0c2b66b92e
IreneLi <zihui.li@ucdconnect.ie>
2023-05-24 11:00:04 +0900

    Update transformer_functions.py

--- wrapper_functions/transformer_functions.py
+++ wrapper_functions/transformer_functions.py
@@ -103,3 +103,87 @@ def get_multi_summary_joint(text, model_name="osama7/t5-summarization-multinews"
     final_summary = '\n\n'.join(final_summary)
 
     return final_summary
+  
+  
+def get_span_answer(question, context,model_name="sultan/BioM-ELECTRA-Large-SQuAD2"):
+    
+    # choices:`sultan/BioM-ELECTRA-Large-SQuAD2`(tested), `deepset/roberta-base-squad2`, `AnonymousSub/SciFive_MedQuAD_question_generation`
+    # result is a dictionary, i.e., {'score': 0.9190713763237, 'start': 34, 'end': 40, 'answer': 'Berlin'}; we return the answer token directly
+    # for testing purpose: question='Where do I live?', context="My name is Wolfgang and I live in Berlin"
+
+    bot =  pipeline(model=model_name)
+    answer = bot(question=question, context=context)
+    return answer['answer']
+    
+def get_question(context, model_name="AnonymousSub/SciFive_MedQuAD_question_generation"):
+    # generate a question given the input content
+    bot =  pipeline(model=model_name)
+    question = bot(context)
+    
+    return question
+
+def get_choice(question, candicates, model="russab0/distilbert-qa"):
+    # multiple-choice-qa there is no fine-tuned version on headQA!, reference: https://huggingface.co/persiannlp/mbert-base-parsinlu-multiple-choice
+    # we return the answer directly
+
+    # from typing import List
+    # import torch
+    # 
+
+    # model_name = "persiannlp/mbert-base-parsinlu-multiple-choice"
+    model_name = model
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    config = AutoConfig.from_pretrained(model_name)
+    model = AutoModelForMultipleChoice.from_pretrained(model_name, config=config)
+
+    assert len(candicates) == 4, "you need four candidates"
+    choices_inputs = []
+    for c in candicates:
+        text_a = ""  # empty context
+        text_b = question + " " + c
+        inputs = tokenizer(
+            text_a,
+            text_b,
+            add_special_tokens=True,
+            max_length=128,
+            padding="max_length",
+            truncation=True,
+            return_overflowing_tokens=True,
+        )
+        choices_inputs.append(inputs)
+
+    input_ids = torch.LongTensor([x["input_ids"] for x in choices_inputs])
+    output = model(input_ids=input_ids)
+   
+    print (question+' choose from:')
+    print (candicates)
+    
+    return candicates[torch.argmax(output['logits'])]
+    
+def get_med_question(context, model_name="AnonymousSub/SciFive_MedQuAD_question_generation"):
+    # generate a question given the input content
+    bot =  pipeline("text2text-generation", model=model_name)
+    question = bot(context)[0]
+    question = question['generated_text']
+    return question
+    
+def get_dialogpt():
+    
+
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+
